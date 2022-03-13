3f0e7eb91df3f1114a07f4b96ae7524401eda369
Naman Jain <44242805+namanjainyr@users.noreply.github.com>
2022-03-13 16:38:07 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable svc_port{
+    default = "8080"
+}
+
+variable alb_name{
+    default = "ELB"
+}
+
+variable target_group_name{
+    default = "TG1"
+}
+
+variable alb_path{
+    default = "/"
+}
+
+variable alb_listener_port{
+    default = "8080"
+}
+
+variable alb_listener_protocol{
+    default = "HTTP"
+}
+
+variable target_group_path{
+    default = "/"
+}
+
+variable target_group_port{
+    default = "8080"
+}
